4d10b7d4811b2eb9460ae8c89f5f7b729ca19a62
B.X.Trưởng <truongbuixuan11@gmail.com>
2024-10-13 16:45:04 +0700

    all

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,9 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/")
     public String index() {
-        return "bxt sama";
+        return "bxt sama dep trai";
     }
-    
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "only user can access this page";
+    }
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "only admin can access this page";
+    }
 }
